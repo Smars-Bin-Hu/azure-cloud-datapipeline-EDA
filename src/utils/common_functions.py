1adb143357aac6567de8f222b4dab1d7f20ad81d
Smars-Bin-Hu <Smars-Bin-Hu>
2025-06-24 04:23:38 +0000

    move mount_adls_storage to utils

--- src/utils/common_functions.py
+++ src/utils/common_functions.py
@@ -110,27 +110,22 @@ def merge_delta_data(input_df, db_name, table_name, folder_path, merge_condition
 # COMMAND ----------
 
 def mount_adls(storage_account_name, container_name):
-    # Get secrets from Key Vault
-    client_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-client-id')
-    tenant_id = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-tenant-id')
-    client_secret = dbutils.secrets.get(scope = 'formula1-scope', key = 'formula1-app-client-secret')
-    
-    # Set spark configurations
-    configs = {"fs.azure.account.auth.type": "OAuth",
-              "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-              "fs.azure.account.oauth2.client.id": client_id,
-              "fs.azure.account.oauth2.client.secret": client_secret,
-              "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"}
-    
+    # # Get secrets from Key Vault
+    account_key = dbutils.secrets.get(scope="smars-project-scope", key="smarsproject0datalake-access-key")
+
+    # # Set spark configurations
+    configs = {f"fs.azure.account.key.{storage_account_name}.blob.core.windows.net": f"{account_key}"}
+
     # Unmount the mount point if it already exists
     if any(mount.mountPoint == f"/mnt/{storage_account_name}/{container_name}" for mount in dbutils.fs.mounts()):
         dbutils.fs.unmount(f"/mnt/{storage_account_name}/{container_name}")
     
     # Mount the storage account container
     dbutils.fs.mount(
-      source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+      source = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net/",
       mount_point = f"/mnt/{storage_account_name}/{container_name}",
-      extra_configs = configs)
+      extra_configs = configs
+    )
     
     display(dbutils.fs.mounts())
   
---
+++ src/utils/mount_adls_storage.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Mount Azure Data Lake Container for the project
+
+# COMMAND ----------
+
+# MAGIC %run "../../utils/common_functions"
+
+# COMMAND ----------
+
+storage_account_name = 'smarsproject0datalake'
+
+# COMMAND ----------
+
+mount_adls(storage_account_name, 'raw')
+
+# COMMAND ----------
+
+mount_adls(storage_account_name, 'processed')
+
+# COMMAND ----------
+
+mount_adls(storage_account_name, 'presentation')
+
+# COMMAND ----------
+
+dbutils.fs.ls(f"/mnt/{storage_account_name}/raw")
+
+# COMMAND ----------
+
+dbutils.fs.ls(f"/mnt/{storage_account_name}/processed")
+
+# COMMAND ----------
+
+dbutils.fs.ls(f"/mnt/{storage_account_name}/presentation")
